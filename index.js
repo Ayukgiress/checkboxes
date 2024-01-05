55aa6b03781bfe426fa5b815f9d41e1e77b2cc2f
AyukGiress <giress865@gmail.com>
2024-01-05 01:37:40 +0100

    fixing some issus

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const cheap = document.querySelector('#cheap')
 const fast = document.querySelector('#fast')
 
 toggles.forEach((toggle) =>
-  toggle.addEventListener("change", (e) => doTheTrick(e.target))
-);
+  toggle.addEventListener('change', (e) => doTheTrick(e.target))
+)
 
 function doTheTrick (theClickedOne) {
   if (good.checked && cheap.checked && fast.checked) {
